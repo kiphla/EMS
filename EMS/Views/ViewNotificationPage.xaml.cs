e580e2dd032b6c047bc2a2cd90093ab1a97509e4
kiphla <87923074+kiphla@users.noreply.github.com>
2025-10-20 18:32:32 +1100

    small fixes to view notificaiton page

--- EMS/Views/ViewNotificationPage.xaml.cs
+++ EMS/Views/ViewNotificationPage.xaml.cs
@@ -2,45 +2,54 @@
 using System.Windows;
 using System.Windows.Controls;
 
-namespace EMS.Views {
-    public partial class ViewNotificationPage : Page {
+namespace EMS.Views
+{
+    public partial class ViewNotificationPage : Page
+    {
         private readonly Notification currentNotification;
 
-        public ViewNotificationPage(Notification notification) {
+        public ViewNotificationPage(Notification notification)
+        {
             InitializeComponent();
             currentNotification = notification;
             LoadNotificationDetails();
         }
 
-        private void LoadNotificationDetails() {
-            //txtTitle.Text = currentNotification.Title;
-            //txtCreationDate.Text = currentNotification.CreationDate.ToShortDateString();
-            //txtTerminationDate.Text = currentNotification.TerminationDate?.ToShortDateString() ?? "Ongoing";
-            //txtDescription.Text = currentNotification.Description;
+        private void LoadNotificationDetails()
+        {
+            txtTitle.Text = currentNotification.title;
+            txtCreationDate.Text = currentNotification.creationDate.ToShortDateString();
+            txtTerminationDate.Text = currentNotification.terminationDate.ToShortDateString() ?? "Ongoing";
+            txtDescription.Text = currentNotification.description;
         }
 
-        private void BtnTerminate_Click(object sender, RoutedEventArgs e) {
-            // TODO: Update notification in repository
+        private void BtnTerminate_Click(object sender, RoutedEventArgs e)
+        {
             // currentNotification.Active = false;
             // _notificationRepo.Update(currentNotification);
 
             Window parentWindow = Window.GetWindow(this);
-            if (parentWindow != null) {
+            if (parentWindow != null)
+            {
                 parentWindow.Close();
             }
         }
 
-        private void BtnHome_Click(object sender, RoutedEventArgs e) {
+        private void BtnHome_Click(object sender, RoutedEventArgs e)
+        {
             Window parentWindow = Window.GetWindow(this);
-            if (parentWindow != null) {
+            if (parentWindow != null)
+            {
                 parentWindow.Close();
             }
         }
 
-        private void BtnLogout_Click(object sender, RoutedEventArgs e) {
+        private void BtnLogout_Click(object sender, RoutedEventArgs e)
+        {
             var logoutWindow = new LogoutWindow();
             Window parentWindow = Window.GetWindow(this);
-            if (logoutWindow.ShowDialog() == true && parentWindow != null) {
+            if (logoutWindow.ShowDialog() == true && parentWindow != null)
+            {
                 Application.Current.MainWindow.Show();
                 parentWindow.Close();
             }
